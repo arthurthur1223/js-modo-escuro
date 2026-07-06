6d143eb4c3184368caea777f5b5b036069093c89
arthurthur1223 <arthur.fernandes.ramos@escola.pr.gov.br>
2026-07-06 12:33:55 -0300

    PARTE 3

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function alterarTexto() {
-    document.getElementById("mensagem").innerText = 
-        "O texto foi alterado usando JavaScript!";
+function aumentarTexto() {
+    document.getElementById("mensagem")
+        .style.fontSize = "30px";
+}
+
+function mudarFonte() {
+    document.getElementById("mensagem")
+        .style.fontFamily = "Courier New";
+}
+
+function negritoTexto() {
+    document.getElementById("mensagem")
+        .style.fontWeight = "bold";
 }
