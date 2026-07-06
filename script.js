9f2a08c1af042b83f491d947ad8578f696488fa5
arthurthur1223 <arthur.fernandes.ramos@escola.pr.gov.br>
2026-07-06 12:32:34 -0300

    PARTE 1

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-function modoClaro() {
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = "black";
-    
+function alterarTexto() {
     document.getElementById("mensagem").innerText = 
-        "Modo claro ativado!";
-}
-
-function modoEscuro() {
-    document.body.style.backgroundColor = "black";
-    document.body.style.color = "white";
-    
-    document.getElementById("mensagem").innerText = 
-        "Modo escuro ativado!";
+        "O texto foi alterado usando JavaScript!";
 }
