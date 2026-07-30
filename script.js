1721f9b1922cc699f9a3bcfd9824dd4c750497fd
arthurthur1223 <arthur.fernandes.ramos@escola.pr.gov.br>
2026-07-30 15:56:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function alterarTexto() {
-    document.getElementById("mensagem").innerText = 
-        "O texto foi alterado usando JavaScript!";
+
+function mostrarNome() {
+    let nome = document.getElementById("nome").value;
+
+    document.getElementById("mensagem").innerText =
+        "Bem-vindo, " + nome + "!";
 }
